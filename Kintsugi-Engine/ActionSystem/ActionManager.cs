8b187f76393f117462825c87cddf2aa55788d3b3
Mati <migogmati@gmail.com>
2024-01-31 12:33:35 +0100

    First iteration

---
+++ Kintsugi-Engine/ActionSystem/ActionManager.cs
@@ -0,0 +1,40 @@
+﻿using Kintsugi.ActionSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ActionSystem
+{
+    public class ActionManager
+    {
+        internal List<BaseAction> ActionQueue = new();
+        private static ActionManager _instance = new();
+
+        public ActionManager I
+        {
+            get => _instance;
+        }
+
+        public void Queue(BaseAction action)
+        {
+            ActionQueue.Insert(ActionQueue.Count, action);
+        }
+        public void QueueImmediate(BaseAction action)
+        {
+            ActionQueue.Insert(0, action);
+        }
+
+        internal void ProcessQueue()
+        {
+            while (ActionQueue.Count > 0)
+            {
+                var currentAction = ActionQueue.First();
+                ActionQueue.RemoveAt(0);
+
+                currentAction.Execute();
+            }
+        }
+    }
+}
---
+++ Kintsugi-Engine/ActionSystem/BaseAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintsugi.ActionSystem
+{
+    public abstract class BaseAction
+    {
+        public abstract void Execute();
+
+        public abstract bool IsFinished();
+    }
+}
