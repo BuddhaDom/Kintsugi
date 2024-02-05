93cb9c191f96bd9450d8833c7b862470e19a6988
Ed <cooljoller@gmail.com>
2024-02-05 15:08:53 +0100

    Create Dice.cs

---
+++ Kintsugi-Engine/Core/Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintsugi.Core
+{
+    public static class Dice
+    {
+        public static int Roll(int amount, int value)
+        {
+            Console.WriteLine("bruh");
+            return 1;
+        }
+    }
+}
