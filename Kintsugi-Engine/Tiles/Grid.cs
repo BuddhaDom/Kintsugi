5184a848479dc0c327fdcb783c1e95f21ddb68b8
Buddha <64340373+BuddhaDom@users.noreply.github.com>
2024-02-05 10:34:19 +0100

    docstring

--- Kintsugi-Engine/Tiles/Grid.cs
+++ Kintsugi-Engine/Tiles/Grid.cs
@@ -4,7 +4,14 @@ using Debug = System.Diagnostics.Debug;
 
 namespace Kintsugi.Tiles;
 
-
+/// <summary>
+/// A <see cref="GameObject"/> containing a set of tiles to be rendered into a scene.
+/// </summary>
+/// <param name="gridWidth">The width (in tiles) of the grid.</param>
+/// <param name="gridHeight">The height (in tiles) of the grid.</param>
+/// <param name="tileWidth"><para>Default: <c>10.0f</c></para>The uniform size of tiles.</param>
+/// <param name="gridVisible"><para>Default: <c>false</c></para><c>true</c> if the grid borders are to be displayed as well.</param>
+/// <param name="gridColor">Color of the grid borders if <paramref name="gridVisible"/> is <c>true</c>.</param>
 public class Grid(int gridWidth, int gridHeight,  float tileWidth = 10.0f, 
     bool gridVisible = false, Color gridColor = default) : GameObject
 {
