8fe6bfc83132940b139777a9313bd6ee3b126804
Ed <cooljoller@gmail.com>
2024-02-13 12:32:59 +0100

    removed print

--- Kintsugi-Engine/Core/Dice.cs
+++ Kintsugi-Engine/Core/Dice.cs
@@ -15,23 +15,22 @@ namespace Kintsugi.Core
             List<int> diceList = new List<int>();
             int result = 0;
             // Generate and display 5 random integers between 0 and 100.
-            Console.WriteLine("Dice roll:");
+            //Console.WriteLine("Dice roll:");
             for (int ctr = 0; ctr <= amount; ctr++)
             {
                 roll = rand.Next(allowZero ? 0 : 1, value + 1);
                 diceList.Add(roll);
-                Console.Write("{0,8:N0}", roll);
+                //Console.Write("{0,8:N0}", roll);
                 if (!grabHighest)
                 {
                     result = result + roll; // add roll
                 }
             }
-            if(grabHighest) { 
-                result = diceList.Max();
-            }
-            Console.WriteLine("\nTotal roll is {0}", result);
-            return result;
 
+            if(grabHighest) result = diceList.Max();
+
+            //Console.WriteLine("\nTotal roll is {0}", result);
+            return result;
         }
         // Skewed Dice Results
         // Random float value
