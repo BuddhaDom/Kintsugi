5fe2349baa17e244210d892c5f545038ea461a92
Mati <migogmati@gmail.com>
2024-02-26 15:26:35 +0100

    Level 1 assets done

--- PuzzleGame/Levels/Level1.cs
+++ PuzzleGame/Levels/Level1.cs
@@ -15,14 +15,14 @@ namespace PuzzleGame.Levels
     {
         public override void Load(Game game)
         {
-            grid = new Grid(game.GetAssetManager().GetAssetPath("TiledTesting\\forestpath.tmx"), gridVisible: true, gridColor: Color.DarkBlue);
+            grid = new Grid(game.GetAssetManager().GetAssetPath("Tiles\\level1.tmx"), gridVisible: true, gridColor: Color.DarkBlue);
             grid.Transform.X = 0;
             grid.Transform.Y = 0;
             Bootstrap.GetCameraSystem().Size = 16 * 10;
 
 
             // TODO: This sucks. Should be attached to grid on the transform itself.
-            var transform = new TileObjectTransform(Vec2Int.One * 3, 0, grid);
+            var transform = new TileObjectTransform(new Vec2Int(0, 5), 3, grid);
             var transform2 = new TileObjectTransform(new Vec2Int(1, 2), 0, grid);
             var collider = new TileObjectCollider([0], [1]);
             var sprite = new TileObjectSprite(game.GetAssetManager().GetAssetPath("guy.png"), Vector2.One / 2,
@@ -30,13 +30,13 @@ namespace PuzzleGame.Levels
             var character = new MovementActor(transform, collider, sprite, "john");
             var character2 = new MovementActor(transform2, collider, sprite, "bob");
             var scenario = new MovingScenario();
-            var group_player = new PlayerControlGroup("PLAYER")
+            var group_player = new PlayerControlGroup("PLAYER");
             var group_environment = new EnvironmentControlGroup("ENVIRONMENT");
 
-            group.AddActor(character);
-            group.AddActor(character2);
+            group_player.AddActor(character);
+            //group.AddActor(character2);
 
-            scenario.AddControlGroup(group);
+            scenario.AddControlGroup(group_player);
             // scenario.AddControlGroup(group2);
 
             scenario.BeginScenario();
