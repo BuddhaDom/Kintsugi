b865119aa89ca8f8789ecdc4dc4f657793707a60
Mati <migogmati@gmail.com>
2024-02-26 15:15:21 +0100

    working on tilemaps

---
+++ PuzzleGame/Assets/Tiles/battle_tilemap_packed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="battle_tilemap_packed" tilewidth="16" tileheight="16" tilecount="198" columns="18">
+ <image source="battle_tilemap_packed.png" width="288" height="176"/>
+</tileset>
