d1fceb64a58d9a7322dfbaf3ee86e5c5943b3e83
Mati <migogmati@gmail.com>
2024-03-14 16:14:10 +0100

    Starting on abilities

---
+++ TacticsGameTest/Abilities/Ability.cs
@@ -0,0 +1,18 @@
+﻿using Kintsugi.Core;
+using Kintsugi.Objects;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticsGameTest.Abilities
+{
+    internal abstract class Ability
+    {
+        public abstract IEnumerable<(Vec2Int, Color)> GetTargets(SelectableActor act, Vec2Int from);
+        public abstract void Hover(Vec2Int target);
+        public abstract void DoAction(Vec2Int target);
+    }
+}
---
+++ TacticsGameTest/Abilities/Stride.cs
@@ -0,0 +1,28 @@
+﻿using Kintsugi.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticsGameTest.Abilities
+{
+    internal class Stride : Ability
+    {
+        public override void DoAction(Vec2Int target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<(Vec2Int, Color)> GetTargets(SelectableActor act, Vec2Int from)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Hover(Vec2Int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
