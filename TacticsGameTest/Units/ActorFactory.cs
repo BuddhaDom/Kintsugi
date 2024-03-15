6b8a94f4eef26f441fff516c8fec166682b3d721
Ed <cooljoller@gmail.com>
2024-03-15 13:14:26 +0100

    base unit

---
+++ TacticsGameTest/Units/ActorFactory.cs
@@ -0,0 +1,53 @@
+﻿using Kintsugi.Core;
+using Kintsugi.Objects;
+using Kintsugi.Objects.Graphics;
+using Kintsugi.Objects.Properties;
+using Kintsugi.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticsGameTest
+{
+    static class ActorFactory
+    {
+        static Animation zombieAnim ;
+        static Animation skeletonAnim;
+        static Animation mummyAnim;
+
+        public static SelectableActor Zombie(Game game, Grid grid)
+        {
+            if (zombieAnim == null)
+            {
+                var spritesheet = new SpriteSheet(
+                    game.GetAssetManager().GetAssetPath("MutilatedStumbler.png"),
+                    16,
+                    16,
+                    4,
+                    Vector2.One / 2,
+                    new Vector2(8f, 8f)
+                    );
+
+                zombieAnim = new Animation(
+                    1f,
+                    spritesheet,
+                    Enumerable.Range(0, 4));
+            }
+
+
+            var character = new MovementActor("Zombie");
+
+            character.SetAnimation(zombieAnim);
+            character.SetCollider(["unit"], ["unit", "flying_unit", "wall", "spikes"], false);
+            character.AddToGrid(grid, 2); // this should be inherited from a generic unit
+            character.SetEasing(TweenSharp.Animation.Easing.QuadraticEaseOut, 0.2f);
+            character.speed = 1;
+            return character;
+        }
+
+    }
+}
---
+++ TacticsGameTest/Units/BaseUnit.cs
@@ -0,0 +1,47 @@
+﻿using Kintsugi.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacticsGameTest.Units
+{
+    internal class BaseUnit :
+        Actor
+    {
+        public int Hp { get; set; }
+        public int MaxHp { get; set; }
+
+        public int Swift { get; set; } = 1;
+
+        public int Intuition { get; set; } = 1;
+
+        public int Brawn { get; set; } = 1;
+
+        public int DamageMelee { get; set; }
+
+        public int DamageRanged { get; set; }
+
+
+        public override void OnEndRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnEndTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnStartRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnStartTurn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
