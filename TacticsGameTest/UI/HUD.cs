ad945248e0fac9db5e27e89ebde7f4299cbc39b6
Buddha <64340373+BuddhaDom@users.noreply.github.com>
2024-03-17 13:06:02 +0100

    i got the dtat

--- TacticsGameTest/UI/HUD.cs
+++ TacticsGameTest/UI/HUD.cs
@@ -8,13 +8,13 @@ namespace TacticsGameTest.UI;
 
 internal class HUD : Canvas
 {
-    private static HUD? _hud = null;
+    private static HUD? hud;
 
     /// <summary>
     /// Get the HUD instance singleton.
     /// </summary>
     /// <returns>The existing or new HUD instance.</returns>
-    public static HUD Instance => _hud ??= new HUD();
+    public static HUD Instance => hud ??= new HUD();
 
     private HUD() => Bootstrap.GetInput().AddListener(this);
     
@@ -27,14 +27,16 @@ internal class HUD : Canvas
         new FramedIcon(
             new Vector2(10f, 10f),
             new SpriteSingle(Bootstrap.GetAssetManager().GetAssetPath("GUI\\characterFrame.png")),
-            actor.Graphic is Animation a ? new Animation(a.TimeLength, 
-                new SpriteSheet(
-                    a.SpriteSheet.Path, 
-                    a.SpriteSheet.Dimensions.x, 
-                    a.SpriteSheet.Dimensions.y, 
-                    a.SpriteSheet.SpritesPerRow
-                    ), 
-                new []{0,1,2,3}) : new SpriteSingle(Bootstrap.GetAssetManager().GetAssetPath("guy.png")),
+            actor.Graphic is Animation a
+                ? new Animation(a.TimeLength,
+                    new SpriteSheet(
+                        a.SpriteSheet.Path,
+                        a.SpriteSheet.Dimensions.x,
+                        a.SpriteSheet.Dimensions.y,
+                        a.SpriteSheet.SpritesPerRow
+                    ),
+                    new[] { 0, 1, 2, 3 })
+                : new SpriteSingle(Bootstrap.GetAssetManager().GetAssetPath("guy.png")),
             Vector2.One * 3, Vector2.One * 5
         ).AddToCanvas(this);
 
@@ -45,7 +47,11 @@ internal class HUD : Canvas
                 + 100 // Spacing
                 * i)).AddToCanvas(this);
 
-        //TODO: Health, statuses.
+        new StatFrame(
+            "TEST", 5,
+            new Vector2(150, 10),
+            new Vector2(3, 3)
+        ).AddToCanvas(this);
     }
 
     public void Clear() => Objects.Clear();
---
+++ TacticsGameTest/UI/StatFrame.cs
@@ -0,0 +1,49 @@
+using System.Drawing;
+using System.Numerics;
+using Kintsugi.Core;
+using Kintsugi.UI;
+
+namespace TacticsGameTest.UI;
+
+public class StatFrame : IHUDObject
+{
+    private CanvasObject Name { get; }
+    private CanvasObject Number { get; }
+    
+    public StatFrame(string name, int value, Vector2 position, Vector2 scale)
+    {
+        Number = new CanvasObject
+        {
+            TextColor = Color.FromArgb(184, 111, 80),
+            FontSize = 24,
+            FontPath = "Fonts\\calibri.ttf",
+            TextPosition = new Vector2(17,13),
+            Text = value.ToString(),
+            Position = position
+        };
+        
+        
+        
+        Number.SetSpriteSingle(Bootstrap.GetAssetManager().GetAssetPath("GUI\\statFrame.png"));
+        Number.Graphic!.Scale = scale;
+        
+        Name = new CanvasObject
+        {
+            TextColor = Color.FromArgb(184, 111, 80),
+            FontSize = 24,
+            FontPath = "Fonts\\calibri.ttf",
+            TextPosition = new Vector2(17,13),
+            Text = name,
+            Position = position + Vector2.UnitX * Number.Dimensions * scale,
+        };
+        
+        Name.SetSpriteSingle(Bootstrap.GetAssetManager().GetAssetPath("GUI\\statName.png"));
+        Name.Graphic!.Scale = scale;
+    }
+
+    public void AddToCanvas(Canvas canvas)
+    {
+        canvas.Objects.Add(Name);
+        canvas.Objects.Add(Number);
+    }
+}
